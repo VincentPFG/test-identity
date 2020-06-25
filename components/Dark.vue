12ba217f7487e4a2bcfd08c80a5223da3bd915f0
VincentPFG <vincentgiraud@live.fr>
2020-06-25 17:02:36 +0200

    initial

---
+++ components/Dark.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-btn icon @click="turnDark">
+    <v-icon>mdi-theme-light-dark</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$vuetify.theme.dark = localStorage.dark
+      ? JSON.parse(localStorage.dark)
+      : matchMedia('(prefers-color-scheme: dark)').matches
+  },
+  methods: {
+    turnDark() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      localStorage.dark = this.$vuetify.theme.dark
+    },
+  },
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list nav="nav">
+        <v-list-item
+          v-for="{ route, name } in nav"
+          :key="name"
+          @click="
+            $vuetify.goTo(route)
+            drawer = defaultDrawer
+          "
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ name }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app="app" color="primary">
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer />
+      <Dark />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <div class="d-flex flex-column justify-space-around">
+          <v-alert type="warning">
+            Ce site est en cours de développement.
+          </v-alert>
+          <slot />
+        </div>
+      </v-container>
+    </v-main>
+    <v-footer app="app" absolute="absolute" color="primary">
+      <v-row justify="center">
+        Développé par Vincent GIRAUD
+      </v-row>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import Dark from '../components/Dark.vue'
+
+export default {
+  components: {
+    Dark,
+  },
+  data() {
+    return {
+      drawer: null,
+      defaultDrawer: null,
+      nav: [],
+    }
+  },
+  mounted() {
+    this.defaultDrawer = this.drawer
+  },
+}
+</script>
