9ab03a79776f02d9e324b00b12ffc7e8b82e489a
VincentPFG <vincentgiraud@live.fr>
2020-07-14 17:19:21 +0200

    Snipcart component

---
+++ components/Snipcart.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="snipcart" hidden :data-api-key="apiKey" />
+</template>
+
+<script>
+export default {
+  props: {
+    apiKey: {
+      type: String,
+      required: true,
+    },
+  },
+  head() {
+    return {
+      link: [
+        { rel: 'preconnect', href: 'https://app.snipcart.com' },
+        { rel: 'preconnect', href: 'https://cdn.snipcart.com' },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.css',
+        },
+      ],
+      script: [
+        {
+          src: 'https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.js',
+          body: true,
+        },
+      ],
+    }
+  },
+}
+</script>
