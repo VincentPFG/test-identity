8163eba6c8848f89a9ad9b7de71f842407816608
VincentPFG <vincentgiraud@live.fr>
2020-07-21 17:09:26 +0200

    Product component

---
+++ components/Product.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn v-bind="attrs" class="snipcart-add-item">
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+const { entries, fromEntries } = Object
+
+export default {
+  computed: {
+    attrs() {
+      return fromEntries(
+        entries(this.$attrs).map(([k, v]) => ['data-item-' + k, v])
+      )
+    },
+  },
+}
+</script>
