6c549542feb97316a193e6186eb008beb014306b
VincentPFG <vincentgiraud@live.fr>
2020-07-23 17:47:26 +0200

    Product2

---
+++ components/Product2.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-btn v-bind="attrs" class="snipcart-add-item">
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+export default {
+  computed: {
+    attrs() {
+      const { customs = [], ...others } = this.$attrs
+      const value = {}
+      for (const key in others) {
+        value['data-item-' + key] = others[key]
+      }
+      for (const [index, custom] of customs.entries()) {
+        for (const key in custom) {
+          value[`data-item-custom${index}-${key}`] = custom[key]
+        }
+      }
+      return value
+    },
+  },
+}
+</script>
