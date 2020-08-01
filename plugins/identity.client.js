a06954cd3a5b218add1ba1dbf5e7e6e3cca37cb9
VincentPFG <vincentgiraud@live.fr>
2020-08-01 13:00:02 +0200

    hgjgh

--- plugins/identity.client.js
+++ plugins/identity.client.js
@@ -2,7 +2,7 @@ import identity from 'netlify-identity-widget'
 
 export default (_, inject) => {
   identity.init({
-    APIUrl: 'https://loving-austin-e86800.netlify.app/.netlify/identity',
+    APIUrl: 'https://test-identity.netlify.app/.netlify/identity',
     locale: 'fr',
   })
   inject('identity', identity)
